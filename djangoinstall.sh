84d15a6bca2ef19429306c8cb49a6d2a0e78d6e0
carlosaguilar1928 <carlosaguilar1928@gmail.com>
2018-03-02 21:31:41 -0800

    Update djangoinstall.sh

--- djangoinstall.sh
+++ djangoinstall.sh
@@ -1,24 +1,27 @@
  #!/bin/bash
    yum install -y python pip
-   pip install -y virtualenv
-   pip install --upgrade pip
-   mkdir /opt/django
-   cd /opt/django
-   yum install -y epel-release
-   yum install -y python34 python-pip 
+   pip install -y virtualenv && pip install --upgrade pip
+   
+   # setting up the django environment
+   mkdir /opt/django && cd /opt/django
+   yum install epel-release -y
+   yum install python34 python-pip -y
    virtualenv -p python3 django
-   cd opt/django
-   source bin/activate
-   pip install django 
+   cd opt/django/django
+   source /opt/django/django/bin/activate 
+   
+   # installing django
+   pip install django -y
+   django-admin --version
    django-admin startproject project1
    chown -R carlosaguilar1928 django
-   ls -l django/django
    source /opt/django/django/bin/activate
    
    myip=( curl https://api.ipify.org )
-   sed -i "s/ALLOWED_HOST = \[\]/ALLOWED_HOSTS = \['"$myip"'\]/g" /opt/django/project1/project/settings.py
+   sed -i "s/ALLOWED_HOST = \[\]/ALLOWED_HOSTS = \['"$myip"'\]/g" /opt/django/django/project1/project1//settings.py
 
-   su - carlosaguilar1928  
+   # start the django service 
+   sudo - carlosaguilar1928  
    /opt/django/django/project1/manage.py runserver 0.0.0.0:8000&
    yum install -y git
    
