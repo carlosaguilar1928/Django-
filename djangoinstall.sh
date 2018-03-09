36c210598b7839937a2750f537e872b2562c1090
carlosaguilar1928 <carlosaguilar1928@gmail.com>
2018-03-08 19:05:53 -0800

    Update djangoinstall.sh

--- djangoinstall.sh
+++ djangoinstall.sh
@@ -18,10 +18,8 @@
    chown -R carlosaguilar1928 /opt/django/*
   
    myip=( curl https://api.ipify.org )
-   sed -i "s/ALLOWED_HOST = \[\]/ALLOWED_HOSTS = \['"$myip"'\]/g" /opt/django/django/project1/project1/settings.py
+   sed -i "s/ALLOWED_HOSTS = \[\]/ALLOWED_HOSTS = \['"$myip"'\]/g" /opt/django/django/project1/project1/settings.py
  
   # start the django service 
-   sudo -u carlosaguilar1928
-   source /opt/django/django/bin/activate
-   /opt/django/django/project1/manage.py runserver 0.0.0.0:8000&
+   sudo -u carlosaguilar1928 -E sh -c "source /opt/django/django/bin/activate && /opt/django/django/project1/manage.py runserver 0.0.0.0:8000&"
    yum install -y git
